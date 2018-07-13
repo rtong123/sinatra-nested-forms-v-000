8f6da92d3c2f172860b084251d8e285ca842e811
Rebecca Tong <rtong@buffalo.edu>
2018-07-13 17:42:39 +0000

    automatically backed up by learn

--- app.rb
+++ app.rb
@@ -14,7 +14,7 @@ module FormsLab
     end
 
     post '/pirates' do
-      @pirates = Pirate.new(params[:pirate])
+      @pirate = Pirate.new(params[:pirate])
 
       params[:pirate][:ships].each do |details|
         Ship.new(details)
