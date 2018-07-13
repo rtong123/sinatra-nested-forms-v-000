3873d78c2efa5fcc9cf24a6aec61998879906f04
Rebecca Tong <rtong@buffalo.edu>
2018-07-13 16:48:52 +0000

    automatically backed up by learn

--- app/models/pirate.rb
+++ app/models/pirate.rb
@@ -1,2 +1,10 @@
 class Pirate
+  attr_reader :name, :weight, :height
+
+  def initialize(params)
+    @name = params[:name]
+    @weight = params[:weight]
+    @height = params[:height]
+  end
+  
 end
