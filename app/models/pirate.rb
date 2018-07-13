cdfa7094a0b781402df479084714642d99b51f14
Rebecca Tong <rtong@buffalo.edu>
2018-07-13 17:19:23 +0000

    automatically backed up by learn

--- app/models/pirate.rb
+++ app/models/pirate.rb
@@ -2,8 +2,9 @@ class Pirate
   attr_reader :name, :weight, :height
 
   PIRATES = []
-  
+
   def initialize(params)
+    binding.pry
     @name = params[:name]
     @weight = params[:weight]
     @height = params[:height]
